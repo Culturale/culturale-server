31bce948b8ac0c2af37edf9cb044ee14450befa0
quimvi <quim.baget@gmail.com>
2023-03-02 18:52:32 +0100

    Set up Jest

---
+++ jest.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  preset: 'ts-jest',
+  testPathIgnorePatterns: ['/node_modules/', '/dist/'],
+  moduleNameMapper: {
+    '\\.(jpg|jpeg|png|svg)$': '<rootDir>/test/assetsTransformer.js',
+    '^~/test/(.*)$': '<rootDir>/test/$1',
+    '^~/storybook/(.*)$': '<rootDir>/storybook/$1',
+    '^~/(.*)$': '<rootDir>/app/$1',
+  },
+  testMatch: ['**/*.test.(ts|tsx)'],
+  collectCoverageFrom: ['src/**/*.{js,ts}'],
+  coveragePathIgnorePatterns: ['<rootDir>/server.ts'],
+  globals: {
+    __TEST__: true,
+  },
+};

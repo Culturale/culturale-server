44b70e6ce534cf7abdce4a40c7cae8236c624f59
quimvi <quim.baget@gmail.com>
2023-03-04 18:26:20 +0100

    Añadir ruta informacion usuario al user router

--- src/infrastructure/api/user-routes.ts
+++ src/infrastructure/api/user-routes.ts
@@ -5,3 +5,7 @@ export const userRouter = express.Router();
 userRouter.get('/', (req: Request, res: Response) => {
   console.log('test user router', req, res);
 });
+
+userRouter.get('/informacion_usuario', (req: Request, res: Response) => {
+  console.log('inf user', req, res);
+});

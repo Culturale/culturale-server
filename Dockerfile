766234a60667c17e59ec874d9631d4780f68d720
quimvi <quim.baget@gmail.com>
2023-03-14 14:35:26 +0100

    Aet up compose to run mongo

--- Dockerfile
+++ Dockerfile
@@ -6,21 +6,19 @@ WORKDIR /app
 
 # Copy package.json and package-lock.json
 COPY package*.json ./
+COPY tsconfig.json ./
+COPY tsconfig-paths-bootstrap.js ./
+
+# Copy app source code
+COPY ./src .
 
 # Install dependencies
 RUN npm install
 # Build the app
 RUN npm build
 
-# Copy app source code
-COPY . .
-
 # Expose port 3000
 EXPOSE 3000
-
-# Set environment variables
-ENV NODE_ENV production
-ENV MONGODB_URI mongodb://mongo:27017/mydb
 
 # Start the app
 CMD ["npm", "start"]
--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,6 @@ import express from 'express';
 import { connect } from 'mongoose';
 import { routes } from '~/infrastructure';
 import * as dotenv from 'dotenv';
-
 dotenv.config();
 
 const app = express();
@@ -12,17 +11,21 @@ app.use(express.json());
 
 app.use('/', routes);
 
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME } = process.env;
+
 async function connectToMongo() {
+  const MONGO_URI = `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}?authSource=admin`;
   console.log('Connecting to database...');
-  await connect('mongodb://test');
-  console.log('Connected to Database');
+  console.log('MONGO URI: ', MONGO_URI);
+  connect(MONGO_URI).then(() => console.log('Connected to Database'));
 }
 
 connectToMongo();
 
-const PORT = process.env.NODE_PORT;
+const PORT = process.env.NODE_LOCAL_PORT;
 
 app.listen(PORT, () => {
+  console.log('TEST');
   console.log('Server is running on port', PORT);
 });
 

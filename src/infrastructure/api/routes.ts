8bffa7e2ec3fd96d7fd56f38fc75aa233f050d4c
elsa.boix <elsaboix23@gmail.com>
2023-03-20 18:38:57 +0100

    EslintFixed3

--- src/infrastructure/api/routes.ts
+++ src/infrastructure/api/routes.ts
@@ -1,7 +1,7 @@
 import express from "express";
 
-import { userRouter } from "./user-routes";
 import { eventRouter } from "./event-routes";
+import { userRouter } from "./user-routes";
 
 export const routes = express.Router();
 

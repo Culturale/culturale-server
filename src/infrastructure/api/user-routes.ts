912f6c5f17cfff98e4dfd7e5a60ede5c66a37224
quimvi <quim.baget@gmail.com>
2023-03-21 22:40:18 +0100

    Add login route to user router

--- src/infrastructure/api/user-routes.ts
+++ src/infrastructure/api/user-routes.ts
@@ -1,19 +1,22 @@
-import type { Request, Response } from "express";
-import express from "express";
+import type { Request, Response } from 'express';
+import express from 'express';
 
-import { UserController } from "~/application";
-import { createUserDto } from "~/infrastructure/dtos";
+import { UserController } from '~/application/controllers';
+import { logIn } from '~/application/use-cases';
+import { createUserDto, loginDto } from '~/infrastructure/dtos';
 
 export const userRouter = express.Router();
 
-userRouter.get("/test-user", (req: Request, res: Response) => {
-  res.setHeader("Content-Type", "application/json");
+userRouter.get('/test-user', (req: Request, res: Response) => {
+  res.setHeader('Content-Type', 'application/json');
   res.status(200);
   res.json({
-    hello: "you",
+    hello: 'you',
   });
 });
 
-userRouter.post("/users/create", createUserDto, UserController.createUser);
+userRouter.post('/users/create', createUserDto, UserController.createUser);
 
-userRouter.get("/users", UserController.getAllUsers);
+userRouter.get('/users', UserController.getAllUsers);
+
+userRouter.post('/login', loginDto, logIn);

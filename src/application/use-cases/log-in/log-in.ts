174bbbfb76b6679f15c612a39080a8cee2074ecf
quimvi <quim.baget@gmail.com>
2023-03-21 22:35:53 +0100

    Implement login use case

--- src/application/use-cases/log-in/log-in.ts
+++ src/application/use-cases/log-in/log-in.ts
@@ -1,3 +1,30 @@
-export function logIn(): void {
-  return null;
+import { Request, Response } from 'express';
+import { UserModel } from '~/domain/entities/user';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+
+export async function logIn(req: Request, res: Response): Promise<void> {
+  try {
+    const user = await UserModel.findOne({ username: req.body.username });
+    if (user) {
+      const isPasswordCorrect = await bcrypt.compare(
+        req.body.password,
+        user.password,
+      );
+      if (isPasswordCorrect) {
+        const token = jwt.sign({ username: user.username }, process.env.SECRET);
+        res.json({ token });
+      } else {
+        res.status(400).json({ error: 'Incorrect password' });
+      }
+    }
+    res.status(400).json({
+      message: `Username ${req.body.username} not found`,
+    });
+  } catch (e) {
+    res.status(500);
+    res.json({
+      error: e,
+    });
+  }
 }

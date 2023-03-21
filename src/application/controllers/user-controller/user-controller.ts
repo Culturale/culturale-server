c7eb31bae09687b5d894b0c57c719b5dbda6d031
quimvi <quim.baget@gmail.com>
2023-03-21 22:29:23 +0100

    Encrypt password in create user

--- src/application/controllers/user-controller/user-controller.ts
+++ src/application/controllers/user-controller/user-controller.ts
@@ -1,16 +1,19 @@
-import type { Request, Response } from "express";
+import type { Request, Response } from 'express';
+import bcrypt from 'bcrypt';
 
-import type { IUser } from "~/domain/entities/user";
-import { UserRepository } from "~/domain/repositories/user-repository/user-repository";
+import type { IUser } from '~/domain/entities/user';
+import { UserRepository } from '~/domain/repositories/user-repository/user-repository';
 
 export class UserController {
   public static async createUser(req: Request, res: Response): Promise<void> {
     try {
-      const user: IUser = req.body;
-      await UserRepository.addUser(user);
+      let user: IUser = req.body;
+      user.password = await bcrypt.hash(req.body.password, 10);
+      const userCreated = await UserRepository.addUser(user);
       res.status(200);
       res.json({
-        message: "user created",
+        message: 'user created',
+        user: userCreated,
       });
     } catch (e) {
       res.status(500);

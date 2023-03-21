757cf95d7a30aaf767744143bc04cdada62b0bd7
quimvi <quim.baget@gmail.com>
2023-03-21 22:39:26 +0100

    Create user dto

--- src/infrastructure/dtos/index.ts
+++ src/infrastructure/dtos/index.ts
@@ -1,2 +1,3 @@
-export * from "./create-user.dto";
-export * from "./create-event.dto";
+export * from './create-user.dto';
+export * from './create-event.dto';
+export * from './login.dto';
---
+++ src/infrastructure/dtos/login.dto.ts
@@ -0,0 +1,31 @@
+import { IsString, IsNotEmpty } from 'class-validator';
+import { validate } from 'class-validator';
+import type { NextFunction, Request, Response } from 'express';
+
+class LoginDto {
+  @IsString()
+  @IsNotEmpty()
+  username: string;
+
+  @IsString()
+  @IsNotEmpty()
+  password: string;
+}
+
+export async function loginDto(
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) {
+  const DTO = new LoginDto();
+  DTO.username = req.body.username;
+  DTO.password = req.body.password;
+
+  const errors = await validate(DTO);
+  if (errors.length) {
+    res.status(400).json({ errors });
+    return;
+  }
+
+  next();
+}

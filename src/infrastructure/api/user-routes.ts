796ba762fdcc625323ec5f3823f52a06afbd2623
quimvi <quim.baget@gmail.com>
2023-03-22 00:13:30 +0100

    linter

--- src/infrastructure/api/user-routes.ts
+++ src/infrastructure/api/user-routes.ts
@@ -1,23 +1,22 @@
-import type { Request, Response } from 'express';
-import express from 'express';
+import type { Request, Response } from "express";
+import express from "express";
 
-import { UserController } from '~/application/controllers';
-import { logIn } from '~/application/use-cases';
-import { createUserDto, loginDto } from '~/infrastructure/dtos';
-
-import { authMiddleware } from '~/infrastructure/middlewares';
+import { UserController } from "~/application/controllers";
+import { logIn } from "~/application/use-cases";
+import { createUserDto, loginDto } from "~/infrastructure/dtos";
+import { authMiddleware } from "~/infrastructure/middlewares";
 
 export const userRouter = express.Router();
 
-userRouter.get('/test-user', authMiddleware, (req: Request, res: Response) => {
+userRouter.get("/test-user", authMiddleware, (req: Request, res: Response) => {
   res.status(200);
   res.json({
-    hello: 'you',
+    hello: "you",
   });
 });
 
-userRouter.post('/users/create', createUserDto, UserController.createUser);
+userRouter.post("/users/create", createUserDto, UserController.createUser);
 
-userRouter.get('/users', UserController.getAllUsers);
+userRouter.get("/users", UserController.getAllUsers);
 
-userRouter.post('/users/login', loginDto, logIn);
+userRouter.post("/users/login", loginDto, logIn);

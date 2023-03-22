b8f4b7eddab489a9f790450d9c60019f7e958cd0
Arnau <arnaumoran@gmail.com>
2023-03-22 18:33:35 +0100

    signup with dto

--- src/application/use-cases/sign-up/sign-up.ts
+++ src/application/use-cases/sign-up/sign-up.ts
@@ -1,7 +1,9 @@
 import { UserController } from "../../controllers/user-controller/user-controller";
+import { createUserDto } from "~/infrastructure/dtos/create-user.dto";
 import { Request, Response } from 'express';
 
 export function signUp (req: Request, res: Response): void {
     // create new user
-    UserController.createUser(req, res);
+    createUserDto(req, res, async() => await UserController.createUser(req, res));
+    ;
 }

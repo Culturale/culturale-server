6c520d323a11d279a9cb3bd9df94a47bd3404d26
Arnau <arnaumoran@gmail.com>
2023-03-25 20:06:37 +0100

    signup update

--- src/application/use-cases/sign-up/sign-up.ts
+++ src/application/use-cases/sign-up/sign-up.ts
@@ -2,13 +2,17 @@ import { UserController } from "../../controllers/user-controller/user-controlle
 import { createUserDto } from "~/infrastructure/dtos/create-user.dto";
 import { Request, Response } from 'express';
 
-export async function signUp(req: Request, res: Response): Promise<void> {
-    // create new user
+export async function signUp(req: Request, res: Response) {
     try {
-        createUserDto(req, res, async () => await UserController.createUser(req, res));
+        // create new user
+        await createUserDto(req, res, () => { });
+        await UserController.createUser(req, res);
+
+        res.status(201).json({
+            message: "User created successfully",
+        });
     } catch (e) {
-        res.status(500);
-        res.json({
+        res.status(500).json({
             error: e,
         });
     }

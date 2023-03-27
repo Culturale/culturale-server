b371ee3f687336e8c8c6e82c56b9839c652fa8a6
Arnau <arnaumoran@gmail.com>
2023-03-27 12:21:57 +0200

    test checking

--- src/domain/entities/user/user.test.ts
+++ src/domain/entities/user/user.test.ts
@@ -8,7 +8,18 @@ describe("User Entity", function () {
     instance = new User("test-username", "test-name","test-password","test-email","test-profilePicture","test-phone", "test-type");
   });
 
-  it("can be created", function () {
-    expect(instance).toMatchSnapshot();
+  it("cant be created", function () {
+    //expect(instance).toMatchSnapshot();
+      it("can be created", function () {
+          expect(instance).toEqual({
+              username: "test-username",
+              name: "test-name",
+              password: "test-password",
+              email: "test-email",
+              profilePicture: "test-profilePicture",
+              phoneNumber: "test-phone",
+              usertype: "test-type"
+          });
+      });
   });
 });

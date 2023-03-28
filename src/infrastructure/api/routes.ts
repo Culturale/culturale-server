80b14d0aabae99114a60a39ecf4e8e4c8df966bf
quimvi <quim.baget@gmail.com>
2023-03-28 19:26:02 +0200

    Merge branch with main

--- src/infrastructure/api/routes.ts
+++ src/infrastructure/api/routes.ts
@@ -2,6 +2,7 @@ import express from 'express';
 
 import { eventRouter } from './event-router';
 import { userRouter } from './user-router';
+import { chatRouter } from './chat-router';
 
 export const routes = express.Router();
 

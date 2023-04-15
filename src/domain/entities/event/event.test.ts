a6f6249e7457e3868a9a55592df42aceb880f358
elsa.boix <elsaboix23@gmail.com>
2023-04-15 18:52:35 +0200

    261-62-62-Subscribe-User-Event

--- src/domain/entities/event/event.test.ts
+++ src/domain/entities/event/event.test.ts
@@ -15,7 +15,6 @@ describe('Event Entity', function () {
   beforeEach(function () {
     const time = Date.now();
     const date = new Date(time);
-    // const chat: IChat = null;
     const eventProps: CreateEventDto  = {codi: 20211006023, denominacio: 'test-denominacio' , descripcio: 'test-descripcio', dataIni: date, dataFi: date, horari: '2h', adress: 'test-adress', url: 'test-adress'};
     instance = new Event(
       eventProps,

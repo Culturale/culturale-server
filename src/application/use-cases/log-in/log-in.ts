31afc8db544889c07b22b2baf0d049bc7d82f198
elsa.boix <elsaboix23@gmail.com>
2023-05-03 09:54:51 +0200

    Info sent it

--- src/application/use-cases/log-in/log-in.ts
+++ src/application/use-cases/log-in/log-in.ts
@@ -16,7 +16,7 @@ export async function logIn(req: Request, res: Response): Promise<void> {
       );
       if (isPasswordCorrect) {
         const token = jwt.sign({ username: user.username }, process.env.SECRET);
-        res.status(200).json({ token });
+        res.status(200).json({ token: token, user: user});
       } else {
         res.status(400).json({ error: 'Incorrect password' });
       }

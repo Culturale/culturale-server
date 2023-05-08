5fb53e8aee0b37f7666ad4e462b0b70b5b7e8cb5
Marta Reluy Llamas <martareluyllamas@Air-de-Marta.home>
2023-05-08 14:12:23 +0200

    añadido dtos

--- src/application/use-cases/unfollow-user/unfollow.ts
+++ src/application/use-cases/unfollow-user/unfollow.ts
@@ -3,10 +3,10 @@ import type { Request, Response } from 'express';
 import { UserController } from '../../controllers/user-controller/user-controller';
 
 
-export async function follow(req: Request, res: Response) {
+export async function unfollow(req: Request, res: Response) {
     try {
         // create new user
-        await UserController.addFollower(req, res);
+        await UserController.Unfollow(req, res);
 
         res.status(201).json({
             message: 'User followed successfully',

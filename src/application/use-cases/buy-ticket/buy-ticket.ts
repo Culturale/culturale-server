b74c5ce87d41e109b97eff27b6f6bb825cc1b991
quimvi <quim.baget@gmail.com>
2023-06-06 10:25:44 +0200

    Parse price correctly

--- src/application/use-cases/buy-ticket/buy-ticket.ts
+++ src/application/use-cases/buy-ticket/buy-ticket.ts
@@ -5,6 +5,22 @@ import type { BuyTicketDto } from '~/infrastructure';
 import { StripeService } from '~/infrastructure/services';
 import { getActor } from '~/utils';
 
+function getValueOfEuros(price: string) {
+  const euroValueRegex = /€(\d+(\.\d{1,2})?)/g;
+  const matches = price.match(euroValueRegex);
+
+  if (matches) {
+    const euroValues = matches.map((match: string) => {
+      const valueString = match.slice(1); // Remove the euro symbol
+      return parseFloat(valueString);
+    });
+
+    return euroValues;
+  } else {
+    return [];
+  }
+}
+
 export async function buyTicket(req: Request, res: Response) {
   try {
     const buyTicketDto: BuyTicketDto = req.body;
@@ -15,9 +31,12 @@ export async function buyTicket(req: Request, res: Response) {
 
     const stripe = new StripeService();
 
+    const euroValues = getValueOfEuros(event.price);
+    const price = euroValues[0] * 100;
+
     const paymentIntent = await stripe.createPayment(
       user.stripeCustomerId,
-      parseFloat(event.price),
+      price,
       'eur',
       {
         eventId,
